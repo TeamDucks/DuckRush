b1a4e1f0d2848ea0ef04b856c2183bc262c90491
Nguyễn Anh Quân <naquan2112@gmail.com>
2017-03-09 18:04:54 +0700

    demo duck can run

--- lib/DemoDuck.lua
+++ lib/DemoDuck.lua
@@ -8,6 +8,16 @@ local DuckSprite = require("lib.SpriteSet")().load("/res/proto-duck.png")
   .createFrame(51, 1, 50, 50)
   .createFrame(1, 1, 50, 50)
   .createFrame(101, 1, 50, 50)
+  .createAnimation("left")
+  .createFrame(301, 1, 50, 50)
+  .createFrame(351, 1, 50, 50)
+  .createFrame(301, 1, 50, 50)
+  .createFrame(401, 1, 50, 50)
+  .createAnimation("right")
+  .createFrame(151, 1, 50, 50)
+  .createFrame(201, 1, 50, 50)
+  .createFrame(151, 1, 50, 50)
+  .createFrame(251, 1, 50, 50)
 
 DuckSprite.setAnimation("straight", 10)
 
@@ -16,10 +26,17 @@ local RoadSprite = require("lib.SpriteSet")().load("/res/proto-road.png")
 local timeTotal = 0
 function DemoDuck.update(dt)
   timeTotal = timeTotal + dt
+  if timeTotal > 10 and timeTotal < 11 then
+    DuckSprite.setAnimation("left", 10)
+  end
+  if timeTotal > 20 and timeTotal < 21 then
+    DuckSprite.setAnimation("right", 10)
+  end
   DuckSprite.update(dt)
 end
 
 function DemoDuck.draw()
+  love.graphics.print( timeTotal, 1, 1)
   DuckSprite.draw(50, 50)
 end
 
